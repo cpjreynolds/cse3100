34c263ff9e9298872ac122d8b85b51f697bfa299
Cole Reynolds <cpjreynolds@gmail.com>
2021-09-11 14:55:30 -0400

    e.c initial attempt

--- ps1/e.c
+++ ps1/e.c
@@ -1,13 +1,22 @@
+#include <math.h>
 #include <stdio.h>
+
+static inline unsigned long factorial(unsigned long n) {
+    unsigned long ret = 1;
+    for (; n > 1; n--) {
+        ret *= n;
+    }
+    return ret;
+}
 
 int main(int argc, char* argv[]) {
     printf("n = ");
     int n;
-    /*TODO: Read in n from the user */
-    unsigned int denom = 1;
-    double e = 1;
-    for (int i = 1; i <= n; i++) {
-        /* TODO: Update denom, update the sum */
+    scanf("%u", &n);
+
+    double e = 0;
+    for (unsigned long k = 0; k <= n; k++) {
+        e += 1.0 / factorial(k);
     }
     printf("e =  %lf\n", e);
 }
