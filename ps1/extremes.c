756585f6274db90841fabb07a0a8bc4d17f9ab58
Cole Reynolds <cpjreynolds@gmail.com>
2021-09-13 15:35:06 -0400

    finished problem set 1

--- ps1/extremes.c
+++ ps1/extremes.c
@@ -1,9 +1,18 @@
-#include <stdio.h>
 #include <math.h>
-int main(int argc, char* argv[]){
-  double x, min = INFINITY, max = -INFINITY;
-  int i = 0;
-  while(scanf("%lf", &x) == 1){
-    /* TODO: Compute the min and max, and printf them */
-  }
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    double x, min = INFINITY, max = -INFINITY;
+    int i = 0;
+    while (scanf("%lf", &x) == 1) {
+        /* TODO: Compute the min and max, and printf them */
+
+        if (x > max)
+            max = x;
+
+        if (x < min)
+            min = x;
+
+        printf("Min=%lf Max=%lf\n", min, max);
+    }
 }
--- ps1/powerMod.c
+++ ps1/powerMod.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 
+// I genuinely couldn't think of a way to do it without recursion.
+int powmod(int n, int x, int m) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    }
+    if (x == 0) {
+        return 1;
+    } else if (x == 1) {
+        return n % m;
+    }
+
+    if (x % 2 == 0) { // even
+        return powmod((n * n) % m, x / 2, m) % m;
+    } else { // odd
+        return n * (powmod((n * n) % m, x / 2, m) % m) % m;
+    }
+}
+
 int main(int argc, char* argv[]) {
     printf("Please enter n, e and m: ");
     int n, e, m;
-    e = 0;
-    if (1) {
+    if (scanf("%d %d %d", &n, &e, &m) == 3) {
+        int r = powmod(n, e, m);
+        printf("%d ** %d mod %d = %d\n", n, e, m, r);
     } else {
         printf("You did not enter three integers. Exiting\n");
     }
