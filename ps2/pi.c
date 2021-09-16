e1c2f89145ba1d30a3966bed968fb43d6d28760c
Cole Reynolds <cpjreynolds@gmail.com>
2021-09-16 13:16:07 -0400

    fixed iterative pi

--- ps2/pi.c
+++ ps2/pi.c
@@ -3,7 +3,8 @@
 
 double leibniz_tail_recurrent(int, double) __attribute__((optimize(2)));
 
-double leibnizRecurrent(int n) {
+double leibnizRecurrent(int n)
+{
     /* TODO: Fill in a recurrent version. That is, compute the term for n plus
      * leibnizRecurrent(n - 1) */
     /*
@@ -19,7 +20,8 @@ double leibnizRecurrent(int n) {
 
 // This is tail recursive when optimization is turned on and won't smash the
 // stack with large `n` like the regular recursive version does.
-double leibniz_tail_recurrent(int n, double sum) {
+double leibniz_tail_recurrent(int n, double sum)
+{
     if (n == 0) {
         return sum + 1.0;
     }
@@ -29,24 +31,27 @@ double leibniz_tail_recurrent(int n, double sum) {
     return leibniz_tail_recurrent(n - 1, sum);
 }
 
-double leibnizIterative(int n) {
+double leibnizIterative(int n)
+{
     /* TODO: Fill in the iterative version. That is, use a loop to compute the
      * summation. */
 
     double sum = 0.0;
-    for (int k = 0, sign = 1; k < n; k++, sign *= -1) {
+    for (int k = 0, sign = 1; k <= n; k++, sign = -sign) {
         sum += sign / (2. * k + 1.);
     }
 
     return sum;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     if (argc != 3) {
         printf("Usage: ./pi <mode> <n>, where <mode> is 1 for recurrent "
                "version, and 2 is for the iterative version, and n is the "
                "upper bound of the summation.\n");
-    } else {
+    }
+    else {
         int mode = 0, n = 0;
         double pi = 0;
         assert(sscanf(argv[1], "%d", &mode) == 1);
@@ -56,7 +61,8 @@ int main(int argc, char* argv[]) {
         assert(n >= 0);
         if (mode == 1) {
             pi = 4 * leibnizRecurrent(n);
-        } else {
+        }
+        else {
             pi = 4 * leibnizIterative(n);
         }
         printf("PI: %f\n", pi);
