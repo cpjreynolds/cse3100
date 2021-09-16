f1fcc467422d1a3fd9268d74e27c2490553f9bb6
Cole Reynolds <cpjreynolds@gmail.com>
2021-09-16 12:34:37 -0400

    finished pi

--- ps2/pi.c
+++ ps2/pi.c
@@ -1,15 +1,44 @@
 #include <assert.h>
 #include <stdio.h>
+
+double leibniz_tail_recurrent(int, double) __attribute__((optimize(2)));
+
 double leibnizRecurrent(int n) {
     /* TODO: Fill in a recurrent version. That is, compute the term for n plus
      * leibnizRecurrent(n - 1) */
-    return 0.;
+    /*
+    if (n == 0) {
+        return 1.0;
+    }
+    double sign = n % 2 ? -1.0 : 1.0;
+    double denom = 2 * n + 1;
+    return sign / denom + leibnizRecurrent(n - 1);
+    */
+    return leibniz_tail_recurrent(n, 0.0);
+}
+
+// This is tail recursive when optimization is turned on and won't smash the
+// stack with large `n` like the regular recursive version does.
+double leibniz_tail_recurrent(int n, double sum) {
+    if (n == 0) {
+        return sum + 1.0;
+    }
+    double sign = n % 2 ? -1.0 : 1.0;
+    double denom = 2 * n + 1;
+    sum += sign / denom;
+    return leibniz_tail_recurrent(n - 1, sum);
 }
 
 double leibnizIterative(int n) {
     /* TODO: Fill in the iterative version. That is, use a loop to compute the
      * summation. */
-    return 0.;
+
+    double sum = 0.0;
+    for (int k = 0, sign = 1; k < n; k++, sign *= -1) {
+        sum += sign / (2. * k + 1.);
+    }
+
+    return sum;
 }
 
 int main(int argc, char* argv[]) {
