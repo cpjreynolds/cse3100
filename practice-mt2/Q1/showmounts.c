895fc605a90af69936e0facf85af11f21ef3ce3e
Cole Reynolds <cpjreynolds@gmail.com>
2021-10-29 10:58:24 -0400

    q1 done

--- practice-mt2/Q1/showmounts.c
+++ practice-mt2/Q1/showmounts.c
@@ -1,8 +1,22 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
-int main(int argc,char* argv[])
+int main(int argc, char* argv[])
 {
-   // TODO
+    FILE* df_out = popen("df -h", "r");
+
+    char grep_cmd[256] = "grep ";
+    assert(argc > 1);
+    strncat(grep_cmd, argv[1], sizeof(grep_cmd) - strlen(grep_cmd) - 1);
+    FILE* grep_in = popen(grep_cmd, "w");
+
+    int ch;
+    while (!feof(df_out) && (ch = fgetc(df_out)) != EOF) {
+        fputc(ch, grep_in);
+    }
+    pclose(df_out);
+    pclose(grep_in);
+    return 0;
 }
-
-
